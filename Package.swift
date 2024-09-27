788deea98fa95c59b61e4e7b0744d86c8743780a
rak-sascha-github <sascha.balkau@rakuten.com>
2024-09-27 11:33:37 +0900

    add package.swift

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FacebookSDK",
+    products: [
+        .library(
+            name: "FacebookCore",
+            targets: ["FacebookCore"]),
+        .library(
+            name: "FacebookLogin",
+            targets: ["FacebookLogin"]),
+        .library(
+            name: "FacebookShare",
+            targets: ["FacebookShare"]),
+    ],
+    targets: [
+        .target(
+            name: "FacebookCore",
+            dependencies: [],
+            path: "Sources/Core"),
+        .target(
+            name: "FacebookLogin",
+            dependencies: ["FacebookCore"],
+            path: "Sources/Login"),
+        .target(
+            name: "FacebookShare",
+            dependencies: ["FacebookCore"],
+            path: "Sources/Share"),
+    ]
+)
